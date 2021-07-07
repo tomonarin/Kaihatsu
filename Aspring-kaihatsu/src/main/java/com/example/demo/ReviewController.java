c34d2cb9b92e9305d2d36dde591a49feb9038f2f
id <id@IN0013388>
2021-07-07 14:56:55 +0900

    レビュー表示作成

--- Aspring-kaihatsu/src/main/java/com/example/demo/ReviewController.java
+++ Aspring-kaihatsu/src/main/java/com/example/demo/ReviewController.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,9 +17,33 @@ public class ReviewController {
 	GenreRepository genreR;
 
 	//全レビュー表示（ログイン時、自分の過去投稿）
-    //@RequestMapping(value="/review")
+    @GetMapping(value="/review")
 	public ModelAndView reviews(ModelAndView mv) {
+    	List<Review> reviewList = reviewR.findAll();
+    	mv.addObject("reviews", reviewList);
 
+    	mv.setViewName("list");
+    	return mv;
+    }
+
+	//映画のレビュー表示
+    @GetMapping(value="/review/movie")
+	public ModelAndView movieReviews(ModelAndView mv) {
+    	List<Review> reviewList = reviewR.findByCategoryCode(1);
+    	mv.addObject("reviews", reviewList);
+
+		mv.setViewName("list");
+
+    	return mv;
+    }
+
+    //本のレビュー表示
+    @GetMapping(value="/review/book")
+	public ModelAndView bookReviews(ModelAndView mv) {
+    	List<Review> reviewList = reviewR.findByCategoryCode(2);
+    	mv.addObject("reviews", reviewList);
+
+		mv.setViewName("list");
     	return mv;
     }
 }
